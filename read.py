8dda713581dd60e4d66138122a154cd249c4a5ce
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-27 11:31:29 -0800

    ignoring useless files, add beam pipeline capturing shards

--- read.py
+++ read.py
@@ -6,5 +6,24 @@
 
 import apache_beam as beam
 import apache_beam.pipeline as pipeline
+from apache_beam.options.pipeline_options import PipelineOptions
 
-pipeline | beam.io.ReadFromText('data/20090629173335.plt')
+
+# What > Remove the first four lines
+# Where > Within a single implicit global event-time window
+# When > Once, when the entire bounded input source has been consumed
+# How > Not relevant
+
+
+p = beam.Pipeline(options=PipelineOptions())
+lines = p | 'ReadMyFile' >> beam.io.ReadFromText('/Users/cramsey/Documents/Distributed Deep Learning/Reading Data/beam_read/read_with_beam/data/data.csv')
+
+lines | 'WriteMyFile' >> beam.io.WriteToText(
+      'beam_data/outputData.csv')
+
+p.run()
+lines
+
+
+
+
